451e22453332fd46c13bcd7e0694435adbf88a36
Abdullah Mzaien <s201540830@kfupm.edu.sa>
2021-02-15 14:47:58 +0300

    Trying to support preact

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,2 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
-type nextRedirectsprop = {
-  href: string;
-  condition?: boolean;
-  fallBack?: string;
-};
-
-export default function NextRedirects({
-  href,
-  condition,
-  fallBack,
-}: nextRedirectsprop) {
-  const router = useRouter();
-  useEffect(() => {
-    condition !== undefined
-      ? router.replace(
-          condition === true ? href : fallBack ? fallBack : router.asPath
-        )
-      : router.replace(href);
-  }, [condition]);
-  return null;
-}
+export { default as Redirects } from "./react";
+export { default as RedirectsP } from "./praect";
---
+++ src/praect.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from "preact/hooks";
+import { useRouter } from "next/router";
+
+type nextRedirectsprop = {
+  href: string;
+  condition?: boolean;
+  fallBack?: string;
+};
+
+export default function RedirectsP({
+  href,
+  condition,
+  fallBack,
+}: nextRedirectsprop) {
+  const router = useRouter();
+  useEffect(() => {
+    condition !== undefined
+      ? router.replace(
+          condition === true ? href : fallBack ? fallBack : router.asPath
+        )
+      : router.replace(href);
+  }, [condition]);
+  return null;
+}
